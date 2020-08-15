f8b33b81fc1da44e172674c35cf3d79e7f2c7441
Timotej Stibilj <timotej.stibilj@gmail.com>
2020-08-15 16:02:33 +0200

    nov vmesnik

--- vislice.py
+++ vislice.py
@@ -1,11 +1,36 @@
 import bottle
 import model
 
-vislice = model.Vislice()
+SKRIVNOST = 'moja skrivnost'
+PISKOTEK = 'idigre'
 
-@bottle.get("/")
-def index():
+vislice = model.Vislice(model.DATOTEKA_STANJE) #objekt razreda Vislice
 
-    return bottle.template("index.tpl")
+@bottle.get('/')
+def index(): 
+    return bottle.template('views/index.tpl')
 
-bottle.run(reloader=True, debug=True)
+@bottle.post('/nova-igra/') #ustvarimo novo igro
+def nova_igra():
+    id_igre = vislice.nova_igra()
+    bottle.response.set_cookie(PISKOTEK, str(id_igre), path='/', secret=SKRIVNOST) #nastavimo piškotek
+    bottle.redirect('/igra/') #preusmerimo na drug naslov
+
+@bottle.get('/igra/') #id igre ki je celo število
+def pokazi_igro(): 
+    id_igre = int(bottle.request.get_cookie(PISKOTEK, secret=SKRIVNOST)) #iz piškotka dobimo id igre
+    igra, stanje = vislice.igre[id_igre]
+    return bottle.template('views/igra.tpl', igra=igra, stanje=stanje)
+
+@bottle.post('/igra/')
+def ugibaj(): 
+    id_igre = int(bottle.request.get_cookie(PISKOTEK, secret=SKRIVNOST))
+    crka = bottle.request.forms.getunicode('crka')
+    vislice.ugibaj(id_igre, crka)
+    bottle.redirect('/igra/')
+
+@bottle.get('/img/<slika>')
+def pokazi_sliko(slika): 
+    return bottle.static_file(slika, root='img') #vrne vsebino te datoteke
+
+bottle.run(reloader=True, debug=True)                         
